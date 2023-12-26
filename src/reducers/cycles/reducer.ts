e805ec750b3e7c1a36ae6716fcf70c169e06aa14
kleber costa almeida <ngustavo80@hotmail.com>
2023-12-25 21:02:53 -0300

    Immerjs

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { produce } from 'immer'
+
 import { ActionTypes } from './actions'
 
 export interface Cycle {
@@ -15,40 +17,43 @@ interface cycleState {
 }
 
 export function cyclesReducer(state: cycleState, action: any) {
+  const currentActiveCycle = state.cycles.findIndex(
+    (cycle) => cycle.id === state.activeCycleId,
+  )
+
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
-      }
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
+      })
 
     case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, completeDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
-      }
+      return produce(state, (draft) => {
+        draft.cycles[currentActiveCycle].completeDate = new Date()
+        draft.activeCycleId = null
+      })
     }
 
     case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, interruptDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
-      }
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, interruptDate: new Date() }
+      //     } else {
+      //       return cycle
+      //     }
+      //   }),
+      //   activeCycleId: null,
+      // } METODO SEM O IMMER
+
+      return produce(state, (draft) => {
+        draft.cycles[currentActiveCycle].interruptDate = new Date()
+        draft.activeCycleId = null
+      })
+
+      // MESMA AÇÃO USANDO O IMMER
     }
 
     default:
